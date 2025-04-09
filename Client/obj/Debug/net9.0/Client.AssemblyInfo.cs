46d271967197bb3c05828f742cba371efaf02825
randika <randhikamadushan@gmail.com>
2025-04-09 23:25:39 +0100

    fix(home): improve table responsiveness on home page

--- Client/obj/Debug/net9.0/Client.AssemblyInfo.cs
+++ Client/obj/Debug/net9.0/Client.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57b15393d94394d634b91b3f9495b34d5c7c06a8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55c02526533252d1fa18922c2d6dcbae5769172a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
