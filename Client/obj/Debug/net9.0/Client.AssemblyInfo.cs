209789871993b759726f44844e4f9b76c02295d1
randika <randhikamadushan@gmail.com>
2025-04-10 23:20:07 +0100

    feat(home): Add dropdown profile image menu

--- Client/obj/Debug/net9.0/Client.AssemblyInfo.cs
+++ Client/obj/Debug/net9.0/Client.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55c02526533252d1fa18922c2d6dcbae5769172a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46d271967197bb3c05828f742cba371efaf02825")]
 [assembly: System.Reflection.AssemblyProductAttribute("Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
