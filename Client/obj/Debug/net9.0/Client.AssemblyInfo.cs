0613a48ae9c7d778dcadbec3362690e7235a3874
randika <randhikamadushan@gmail.com>
2025-04-16 23:47:21 +0100

    feat(nav): add new navigation menu with links

--- Client/obj/Debug/net9.0/Client.AssemblyInfo.cs
+++ Client/obj/Debug/net9.0/Client.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+209789871993b759726f44844e4f9b76c02295d1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eac7c32f7948ad37f3fd7e634fd4c28166a324d8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
