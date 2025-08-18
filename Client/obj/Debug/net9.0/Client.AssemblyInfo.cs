905a87157af94d036b56d3e4234d5a5a962de6f8
randika <randhikamadushan@gmail.com>
2025-08-18 22:36:19 +0100

    fix(Employee-Edit/Add): two-step form validation only applies to current step

--- Client/obj/Debug/net9.0/Client.AssemblyInfo.cs
+++ Client/obj/Debug/net9.0/Client.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bac57884efc726707fbdac5df2ef2e7e54593769")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e1a33d56987ca7c083c2fddf6c1cb3a6e79d255")]
 [assembly: System.Reflection.AssemblyProductAttribute("Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
